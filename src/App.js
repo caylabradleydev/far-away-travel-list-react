ab04256b11951a5daeb12b9d099d402fcebee5e0
Cayla Bradley <caylabradleydev@gmail.com>
2024-07-01 21:12:17 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import "./index.css";
+
 function App() {
   return (
-    <div>
-      <h1> Hello World</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>🏖️ Far Away</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-Form">
+      <h3>What do you need for your trip 😍</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
 
 export default App;
